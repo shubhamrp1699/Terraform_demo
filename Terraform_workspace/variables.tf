5a5da05e31323091ab64edba2abf2f0a5b9bdcff
shubhamrp1699 <73349951+shubhamrp1699@users.noreply.github.com>
2021-07-04 20:28:06 +0530

    Update variables.tf

--- Terraform_workspace/variables.tf
+++ Terraform_workspace/variables.tf
@@ -1,3 +1,9 @@
+variable "access_key" {
+  default = "AKIAY7PTQLLBYJFOTF6Q"
+  }
+variable "secret_key" {
+  default = "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"
+  }
 variable "region" {
   default = "ap-south-1"
-}
+  }
