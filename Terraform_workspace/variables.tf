cfff83cfac4d797fbbafabded7063d3d5be302c2
shubhamrp1699 <73349951+shubhamrp1699@users.noreply.github.com>
2021-07-04 20:41:48 +0530

    Delete variables.tf

--- Terraform_workspace/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "access_key" {
-  default = "AKIAY7PTQLLBYJFOTF6Q"
-  }
-variable "secret_key" {
-  default = "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"
-  }
-variable "region" {
-  default = "ap-south-1"
-  }
